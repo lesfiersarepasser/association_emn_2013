92ecb2fbd758555d82342d90639f314d8b1322c1
bourgeskevin <bourges.kevin@gmail.com>
2014-02-17 09:43:23 +0100

    Added Action.java added orm to use schema APP

--- src/lesfiersarepasser/assoc/bdd/DerbyConnection.java
+++ src/lesfiersarepasser/assoc/bdd/DerbyConnection.java
@@ -1,9 +1,29 @@
 package lesfiersarepasser.assoc.bdd;
 
+import java.sql.DriverManager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import lesfiersarepasser.assoc.entities.Article;
+
+import org.apache.derby.client.am.Connection;
+
 
 
 public class DerbyConnection {
-
+	
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Boss_Final_TP");
+		EntityManager em = emf.createEntityManager();
+		
+		
+		em.find(Article.class, "REF01");
+		
+	}
 	
 	
 	
---
+++ src/lesfiersarepasser/assoc/servlet/Action.java
@@ -0,0 +1,46 @@
+package lesfiersarepasser.assoc.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Action
+ */
+public class Action extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Action() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (request.getParameter("login") != null) {
+
+		}
+		if (request.getParameter("register") != null) {
+
+		}
+	}
+
+}
