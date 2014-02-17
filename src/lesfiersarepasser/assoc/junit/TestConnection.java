67c245f7d0c69426eaef4b49829bee4064486c59
lesfiersarepasser <lesfiersarepasser@gmail.com>
2014-02-17 11:18:01 +0100

    package junit

---
+++ src/lesfiersarepasser/assoc/junit/TestConnection.java
@@ -0,0 +1,14 @@
+package lesfiersarepasser.assoc.junit;
+
+import org.junit.Test;
+
+import lesfiersarepasser.assoc.bdd.DerbyConnection;
+
+public class TestConnection{
+
+	@Test
+	public void testTimer1(){
+		
+		//assertTrue("Utilisateur connecté", DerbyConnection.connectUser());
+	}
+}
